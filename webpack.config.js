db7754adee235255a7adb9c2a40c8ffaa04bc615
Maksim Sinelnikau <maksim.sinelnikau@gmail.com>
2023-04-27 22:53:57 +0300

    feat: setting up webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: [{ loader: 'html-loader', options: { minimize: true } }]
+			},
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader',
+              { loader: 'sass-resource-loader',	options: { resources: ['src/styles/vars.scss'] }}
+        ]
+      }
+		]
+	},
+	plugins: [
+    new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+  ],
+	devServer: {
+		compress: true,
+		port: 3000,
+	},
+};
