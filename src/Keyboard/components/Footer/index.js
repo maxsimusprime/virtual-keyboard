811c2c2717a55b5a299e615e803f7ee0c32b3ae4
Maksim Sinelnikau <maksim.sinelnikau@gmail.com>
2023-05-01 21:55:08 +0300

    feat: update Keyboard module

---
+++ src/Keyboard/components/Footer/index.js
@@ -0,0 +1,5 @@
+import createElementFromHTML from '../../../utils/createElementFromHTML';
+import FooterHTML from './index.html';
+import './index.scss';
+
+export default () => createElementFromHTML(FooterHTML);
--- src/Keyboard/index.js
+++ src/Keyboard/index.js
@@ -1,9 +1,23 @@
 import createElementFromHTML from '../utils/createElementFromHTML';
 import KeyboardHTML from './index.html';
 import './index.scss';
+import Textarea from '../components/Textarea';
+import Footer from './components/Footer';
 import Keys from '../Keys';
 
 const keyboard = createElementFromHTML(KeyboardHTML);
-keyboard.appendChild(Keys);
+const textarea = Textarea();
+const footer = Footer();
+const keys = new Keys(textarea);
+
+keyboard.appendChild(textarea);
+keyboard.appendChild(keys.$element);
+keyboard.appendChild(footer);
+
+// textarea.addEventListener('input', (e) => {
+//   console.log('textarea: ', e);
+// });
+document.addEventListener('keydown', (e) => { keys.keydownHandler(e); });
+document.addEventListener('keyup', (e) => { keys.keyupHandler(e); });
 
 export default keyboard;
