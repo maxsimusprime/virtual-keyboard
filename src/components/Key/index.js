40bc85ce07c6339d3fe50a2b430d6d623c8c3495
Maksim Sinelnikau <maksim.sinelnikau@gmail.com>
2023-05-02 00:02:05 +0300

    feat: update Key module

--- src/components/Key/index.js
+++ src/components/Key/index.js
@@ -4,17 +4,34 @@ import './index.scss';
 
 class Key {
   constructor(lang, keyObj) {
+    this.keyObj = keyObj;
     this.$element = createElementFromHTML(KeyHTML);
     this.$element.id = keyObj.code;
     this.$element.classList.add(`key_size_${keyObj.size}`);
     this.$element.textContent = keyObj[lang].key;
   }
 
-  showId() {
-    return this.$element.id;
-  }
-
-  keyDown() {
+  keyDown(target) {
+    if (target && !this.keyObj.actionkey) {
+      target.write(this.$element.textContent);
+    }
+    if (target && this.keyObj.actionkey) {
+      switch (this.$element.id) {
+        case 'Backspace':
+          target.backspace();
+          break;
+        case 'Tab':
+          target.write('\t');
+          break;
+        case 'Delete':
+          target.delete();
+          break;
+        case 'Enter':
+          target.write('\n');
+          break;
+        default:
+      }
+    }
     this.$element.classList.add('key_active');
   }
 
