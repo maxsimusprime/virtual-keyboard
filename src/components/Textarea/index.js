b9fb582273f12c2c3bcc3529cef666413f0fecb4
Maksim Sinelnikau <maksim.sinelnikau@gmail.com>
2023-05-02 01:13:34 +0300

    feat: update Textarea module

--- src/components/Textarea/index.js
+++ src/components/Textarea/index.js
@@ -2,4 +2,51 @@ import createElementFromHTML from '../../utils/createElementFromHTML';
 import TextareaHTML from './index.html';
 import './index.scss';
 
-export default () => createElementFromHTML(TextareaHTML);
+class Textarea {
+  constructor() {
+    this.$element = createElementFromHTML(TextareaHTML);
+  }
+
+  write(char) {
+    this.$element.focus();
+    const { selectionStart, selectionEnd, value } = this.$element;
+    if (selectionStart === selectionEnd) {
+      const firstPart = value.substring(0, selectionStart);
+      const secondPart = value.substring(selectionEnd);
+      const newValue = firstPart + char + secondPart;
+      this.$element.value = newValue;
+      this.$element.selectionStart = selectionStart + 1;
+      this.$element.selectionEnd = selectionStart + 1;
+    }
+  }
+
+  backspace() {
+    this.$element.focus();
+    const { selectionStart, selectionEnd, value } = this.$element;
+    if (selectionStart === selectionEnd) {
+      const firstPart = value.substring(0, selectionStart - 1);
+      const secondPart = value.substring(selectionEnd);
+      const newValue = firstPart + secondPart;
+      this.$element.value = newValue;
+      this.$element.selectionStart = selectionStart - 1;
+      this.$element.selectionEnd = selectionStart - 1;
+    }
+  }
+
+  delete() {
+    this.$element.focus();
+    const { selectionStart, selectionEnd, value } = this.$element;
+    if (selectionStart === selectionEnd) {
+      if (selectionStart < value.length) {
+        const firstPart = value.substring(0, selectionStart);
+        const secondPart = value.substring(selectionStart + 1);
+        const newValue = firstPart + secondPart;
+        this.$element.value = newValue;
+        this.$element.selectionStart = selectionStart;
+        this.$element.selectionEnd = selectionStart;
+      }
+    }
+  }
+}
+
+export default Textarea;
